dfc7f61ff43ef47fb98069fc1b4a54039605206b
zzhhaa <zzhhaa@protonmail.com>
2024-03-05 18:24:08 +0000

    Pulled new changes from main

---
+++ test/error.test.js
@@ -0,0 +1,11 @@
+// Import necessary modules and files
+const { handleMessage } = require('../index.js'); // handleMessage function is exported from index.js
+const { test } = require('node:test');
+
+test("imageUrl is null") // message.content.startswith('!image') will need to be true, and imageUrl will need to be null
+
+test("No message was generated")
+
+test("Error while calling OpenAI")
+
+test("banError")
